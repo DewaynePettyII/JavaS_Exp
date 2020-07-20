92235a2c6f31dd9161517dc804c9bf3262414d9f
Dewayne Petty II <christsiblings@gmail.com>
2020-07-20 09:04:38 -0500

    first JavaS commit

---
+++ java_script.js
@@ -0,0 +1,9 @@
+//Create an array, print the length
+let fruits=['Apple','Banana','Orange'] 
+console.log(fruits.length)
+
+//Access an array item using the index position
+let first=fruits[0]
+console.log(first)
+let last=fruits[fruits.length-1]
+console.log(last)
